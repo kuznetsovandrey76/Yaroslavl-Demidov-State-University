324764adc09821d48df664f27b30ef0658bd0a07
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-06-24 21:37:27 +0300

    add header in right block

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 $(document).ready(function(){
-    var temp = '';
-    for(var i = 0; i < list_of_disciplines.length; i++) {
-      temp += ('<li><a href="#' + list_of_disciplines[i][0] + '">' + list_of_disciplines[i][1] + '</a></li>');
-    }
-    $('.header-list').html(temp)
+	var temp = '';
+	for(var i = 0; i < list_of_disciplines.length; i++) {
+		temp += ('<li><a href="#' + list_of_disciplines[i][0] + '">' + list_of_disciplines[i][1] + '</a></li>');
+	}
+	$('.header-list').html(temp)
+
+	// Добавляем (Обновляем) заголовок в right блоке 
+	$('.header-list li a').click(function() {
+		$('.left h1').html(''); 
+		$('.right h1').html(list_of_disciplines[number_of_disciplines.indexOf(this.text)][2]); 
+
+		// Собираем контент блока - left-list
+		var temp = '';
+		for(var i = 0; i < sections_of_the_discipline[this.text].length; i++) {
+			temp += ('<li><a href="' + sections_of_the_discipline[this.text][i][1] + '">' + sections_of_the_discipline[this.text][i][0] + '</a></li>');
+		}
+		$('.left-list').html(temp);
 
 
-  $('.header-list li a').click(function() {
-    $('.left h1').html(this.text); 
-
-    // Собираем контент блока - left-list
-    var temp = '';
-    for(var i = 0; i < sections_of_the_discipline[this.text].length; i++) {
-      temp += ('<li><a href="' + sections_of_the_discipline[this.text][i][1] + '">' + sections_of_the_discipline[this.text][i][0] + '</a></li>');
-    }
-    $('.left-list').html(temp);
-    
-    $('.left-list li a').click(function() {
-      console.log('ok');
-    });
-  });
+		$('.left-list li a').click(function() {
+			console.log('ok');
+		});
+	});
 
 
-  $('.right').load('templates/limits.html', function(response, status, xhr) {
-      if(status == "error") {
-         $(".right").html("An error occured: " + xhr.status + " " + xhr.statusText);
-      }
-  });
-
+	$('.right-content').load('templates/limits.html', function(response, status, xhr) {
+		if(status == "error") {
+			$(".right-content").html("An error occured: " + xhr.status + " " + xhr.statusText);
+		}
+	});
 
 });
